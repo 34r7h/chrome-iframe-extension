33035bcd28afab89fb5d3ac879223a5828513dbb
irthos <irth03@gmail.com>
2014-11-25 18:14:22 -0800

    nickstits

--- irth.js
+++ irth.js
@@ -1,21 +1,14 @@
 'use strict';
-angular.module('irth', ['firebase'])
-.controller('ctrl', function($scope, $window, $firebase){
-		var dbURL = 'https://tezt.firebaseio.com';
-		var ref = new Firebase(dbURL + '/life');
-		var sync = $firebase(ref);
-		$scope.syncObject = sync.$asObject();
-		$scope.syncArray = sync.$asArray();
+angular.module('irth', [])
+.controller('ctrl', function($scope, $window){
+
 		$scope.back = function(){
 			$window.history.back();
 		};
 		$scope.forward = function(){
 			$window.history.forward();
 		};
-	$scope.exerciseBeGone = 'display:none;';
-	$scope.dietBeGone = 'display:none;';
-	$scope.dayBeGone = 'display:none;';
-	$scope.activityBeGone = 'display:none;';
+
 
 	$scope.newCurrentProjectUrl = function(url){
 		if (url[4] !== ':' && url[5] !== ':'){
@@ -26,33 +19,12 @@ angular.module('irth', ['firebase'])
 		}
 		$scope.browserURL = $scope.currentProjectUrl;
 
-		sync.$push($scope.currentProjectUrl).then(function(newChildRef){
-			setTimeout(function(){
-				if ($scope.syncArray.splice(-1)[0].$value === $scope.syncArray.splice(-2)[0].$value) {
-					sync.$remove(newChildRef.name());
-				} else {
-					return;
-				}
-			},1500)
 
-		});
 
 
 	};
 
 
-	$scope.hideAll = function () {
-		$scope.searchBeGone = 'display:none;';
-		$scope.activityBeGone = 'display:none;';
-		$scope.exerciseBeGone = 'display:none;';
-		$scope.dietBeGone = 'display:none;';
-		$scope.dayBeGone = 'display:none;';
-	};
-
-		setTimeout(function(){
-			var lastURL = $scope.syncArray.splice(-1)[0].$value;
-			$scope.newCurrentProjectUrl(lastURL);
-		},1000);
 })
 	.filter('trustAsResourceUrl', ['$sce', function($sce) {
 		return function(val) {
